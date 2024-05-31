ced4414ec90fa4ed45740b44a762730a181065d0
Jadow44 <147936769+Jadow44@users.noreply.github.com>
2024-05-31 11:32:43 +0200

    Delete prog_principal directory

--- prog_principal/Conf/key.py
+++
@@ -1,6 +0,0 @@
-# Conf/key.py 8Rn9fXkYe93hzKdZ1kA2z1XZJhxVF2nQ1EqtGjCY
-
-SEMANTIC_SCHOLAR_API_KEY = '8Rn9fXkYe93hzKdZ1kA2z1XZJhxVF2nQ1EqtGjCY'
-CROSSREF_API_KEY = 'nul'  # Set to 'nul' if not available
-PUBMED_API_KEY = 'nul'  # Set to 'nul' if not available
-
